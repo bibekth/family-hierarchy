8662f161879dc4f8877c85739691c4159f909ac2
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-07-18 13:39:24 +0545

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1587,8 +1587,8 @@
 
 
     @if (Route::has('login'))
-        <div class="h-8 flex justify-end md:justify-end justify-center px-4 py-2">
-            <div class="flex flex-wrap gap-2 text-sm">
+        <div class="fixed bottom-0 left-0 w-full bg-white dark:bg-[#1b1b18] z-50 px-4 py-2">
+            <div class="flex flex-col md:flex-row justify-center md:justify-end flex-wrap gap-2 text-sm">
                 <a href="{{ route('privacy-policy') }}"
                     class="inline-block px-4 py-2 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm leading-normal whitespace-nowrap">
                     Privacy & Policy
