9f9cfb5e9758ff508c28bef3b8335f394c136d11
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-07-18 13:49:47 +0545

    fixed bootom

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1587,8 +1587,8 @@
 
 
     @if (Route::has('login'))
-        <div class="fixed bottom-0 left-0 w-full bg-white dark:bg-[#1b1b18] z-50 px-4 py-2">
-            <div class="flex flex-col md:flex-row justify-center md:justify-end flex-wrap gap-2 text-sm">
+        <div class="h-auto flex justify-end md:justify-end justify-center px-4 py-2 fixed bg-[#FDFDFC] dark:bg-[#0a0a0a]" style="bottom: 0px;">
+            <div class="flex flex-wrap gap-2 text-sm">
                 <a href="{{ route('privacy-policy') }}"
                     class="inline-block px-4 py-2 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm leading-normal whitespace-nowrap">
                     Privacy & Policy
