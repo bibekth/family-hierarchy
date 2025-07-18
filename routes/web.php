c6cea32b2940f0c0b71d499cadf6ff137bbee586
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-07-18 09:26:19 +0545

    email verification

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\HierarchyController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -10,8 +12,23 @@ Route::get('dashboard', function () {
 
 Auth::routes();
 
-Route::group(['middleware' => 'auth'], function(){
-    // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.resend');
+
+Route::group(['middleware' => ['auth','verified']], function(){
     Route::get('/home', function(){
         return redirect()->route('hierarchy.index');
     })->name('home');
